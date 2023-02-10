db583252fb87f0d2f9cd3385420a5a0dee5db74c
David <pablodavidhuamani@gmail.com>
2023-02-10 13:13:39 -0500

    fix:add responsive table consultas

--- admin/views/consultas/index.php
+++ admin/views/consultas/index.php
@@ -7,17 +7,18 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="public/css/consultas.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-  <link rel="stylesheet" href="public/css/contactanos.css">
 </head>
 <body>
   <?php require_once("views/layouts/navbar.php");?>
-  <main>
-    <header style="display: flex;">
-      <h1>Listado de Consultas</h1>
+  <main class="">
+    <header>
+      <h1 class="text-center">Listado de Consultas</h1>
     </header>
+<div class="table-responsive px-5 ">
 
-    <table class="table">
+    <table class="table table-striped table-bordered">
       <thead>
         <tr>
             <th scope="col">#</th>
@@ -83,6 +84,7 @@
         }?>
       </tbody>
     </table>
+  </div>
     <div class="paginador">
 			<ul>
 			<?php 
