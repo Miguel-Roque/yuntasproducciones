c0c41ee9e25d2d804511e8e3b1f018d72cfcc8cc
David <pablodavidhuamani@gmail.com>
2023-02-16 13:02:27 -0500

    Update Vista_Editar.php

--- admin/views/blog/Vista_Editar.php
+++ admin/views/blog/Vista_Editar.php
@@ -44,7 +44,7 @@
             </div>
 
             <button type="submit" class="btn btn-primary">Editar</button>
-            <button type="submit" class="btn btn-danger">Editar</button>
+            <button type="submit" class="btn btn-danger">Cancelar</button>
         </form>
     </div>
 
