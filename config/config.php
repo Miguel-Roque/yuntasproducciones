3485b117308c93c92d4e1e5fb9850d851a8b40d2
David <pablodavidhuamani@gmail.com>
2023-02-20 11:07:17 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,8 +2,8 @@
 //CONTIENE CONSTANTES PHP
 
 //URL ABSOLUTA (ESTATICA)
-//define("URL", "http://yuntasproducciones.com/");
-define("URL", "http://localhost/YUNTAS_Producciones/");
+define("URL", "http://yuntasproducciones.com/");
+//define("URL", "http://localhost/YUNTAS_Producciones/");
 
 
 ?>
