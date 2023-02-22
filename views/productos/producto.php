8d15cfa726796ab7fbfc7a1e96660a302a432447
David <pablodavidhuamani@gmail.com>
2023-02-21 20:35:39 -0500

    update_modal

--- views/productos/producto.php
+++ views/productos/producto.php
@@ -44,49 +44,209 @@
       </div>
     </div>
 
-    <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="myModalLabel" aria-hidden="true">
+<style>
+ .modal-content {
+  background-color: rgba(0, 0, 0, 0.7);
+  border: none;
+  border-radius: 10px;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.3), 0px 0px 40px rgba(0, 0, 0, 0.1);
+  padding: 30px;
+  position: relative;
+  text-align: center;
+  color: #fff;
+  font-weight: bold;
+  font-size: 20px;
+}
+
+.modal-header {
+  border: none;
+}
+
+.modal-title {
+  color: #fff;
+  font-size: 30px;
+}
+
+.form-label {
+  font-size: 24px;
+  color: #fff;
+}
+
+.form-cont {
+  background-color: rgba(0, 0, 0, 0.5);
+  border: none;
+  border-radius: 10px;
+  color: #fff;
+  font-size: 18px;
+  margin-bottom: 20px;
+  padding: 15px;
+  border-bottom: 1px solid #ffffff;
+}
+
+.form-cont:focus {
+  box-shadow: none;
+}
+
+.btn-primari {
+  background-color: #21c7c2;
+  border: none;
+  display: block;
+  margin: 0 auto;
+  border-radius: 30px;
+  color: #fff;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 15px 50px;
+  text-transform: uppercase;
+  transition: 0.5s ease-in-out;
+}
+
+.btn-primari:hover {
+  display: block;
+  margin: 0 auto;
+  background-color: #fff;
+  color: #21c7c2;
+  box-shadow: 0px 0px 20px #21c7c2;
+}
+
+.btn-close {
+  position: absolute;
+  right: 0;
+  top: 0;
+}
+
+.btn-close:hover {
+  color: #21c7c2;
+  cursor: pointer;
+}
+
+.modal-footer {
+  border: none;
+  margin-top: 30px;
+  position: relative;
+}
+
+.modal-footer::before {
+  background: linear-gradient(90deg, #00d2ff, #3a7bd5, #00d2ff);
+  content: "";
+  height: 4px;
+  position: absolute;
+  top: -4px;
+  width: 100%;
+}
+
+.modal-footer::after {
+  background: linear-gradient(90deg, #00d2ff, #3a7bd5, #00d2ff);
+  content: "";
+  height: 4px;
+  position: absolute;
+  bottom: -4px;
+  width: 100%;
+}
+
+@media (max-width: 576px) {
+  .modal-content {
+    padding: 15px;
+  }
+
+  .form-label {
+    font-size: 18px;
+  }
+
+  .form-cont {
+    font-size: 16px;
+    margin-bottom: 10px;
+    padding: 10px;
+  }
+
+  .btn-primari {
+    font-size: 18px;
+    padding: 10px 30px;
+  }
+
+  .btn-close {
+    font-size: 30px;
+  }
+}
+
+
+  </style>
+<div class="modal fade " id="myModal" tabindex="-1" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-dialog">
-    <div class="modal-content">
+    <div class="modal-content ">
       <div class="modal-header">
         <h5 class="modal-title" id="myModalLabel">Formulario de contacto</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <button type="button" class="btn-close bg-white" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
-        <form>
+      <div class="modal-body ">
+        <form action="enviarEmergente" method="post" enctype="multipart/form-data">
           <div class="mb-3">
             <label for="nombre" class="form-label">Nombre</label>
-            <input type="text" class="form-control" id="nombre">
+            <input name="nom" type="text" class="form-cont form-control " id="inputNombre">
+          </div>
+          <div class="mb-3">
+            <label for="apellidos" class="form-label">Apellidos</label>
+            <input name="ape" type="text" class="form-cont form-control" id="inputApellido">
           </div>
           <div class="mb-3">
             <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email">
+            <input name="correo" type="text" class="form-cont form-control" id="inputCorreo">
           </div>
           <div class="mb-3">
-            <label for="mensaje" class="form-label">Mensaje</label>
-            <textarea class="form-control" id="mensaje" rows="3"></textarea>
+            <label for="Celular" class="form-label">Celular</label>
+            <input name="cel" type="text" class="form-cont form-control" id="inputCelular" >
           </div>
+          <div class="modal-footer">
+          <button type="submit" class="btn btn-primari" data-bs-dismiss="modal" >Enviar</button>
+        </div>
         </form>
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-        <button type="button" class="btn btn-primary">Enviar</button>
-      </div>
+      
     </div>
   </div>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 
 <script>
-  var myModalEl = document.getElementById('myModal');
   $(document).ready(function() {
-  
+    var modalShown = localStorage.getItem('modalShown');
 
-  setTimeout(function() {
-    // Mostrar el modal
-    $('#myModal').modal('show');
-  }, 5000);
-});
+    if (!modalShown) {
+      setTimeout(function() {
+        $('#myModal').modal('show');
+        localStorage.setItem('modalShown', 'true');
+      }, 5000);
+    }
+
+    $('#formulario').on('submit', function(e) {
+      e.preventDefault();
+
+      // Validar los campos
+      var nombre = $('#nombre').val();
+      var apellidos = $('#apellidos').val();
+      var email = $('#email').val();
+      var celular = $('#celular').val();
+
+      if (!nombre || !apellidos || !email || !celular) {
+        swal('Error', 'Por favor complete todos los campos', 'error');
+      } else {
+        // Enviar los datos del formulario
+        $.ajax({
+          url: 'enviar.php',
+          method: 'POST',
+          data: $(this).serialize(),
+          success: function(response) {
+            swal('¡Gracias!', 'Tu mensaje ha sido enviado', 'success');
+          },
+          error: function() {
+            swal('Error', 'Ocurrió un error al enviar el mensaje', 'error');
+          }
+        });
+      }
+    });
+  });
 </script>
 
     <div class="info-2 info-2-first">
